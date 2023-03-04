8aecc4fecde92d268d15aadfee7ef5ada5b2d92e
Abiodun Adefila <adefaze@gmail.com>
2023-03-04 21:45:57 +0100

    error message added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 // import { onMounted } from "vue";
-import { initModals } from "flowbite";
+// import { initModals } from "flowbite";
 
 // initialize components based on data attribute selectors
 // onMounted(() => {
@@ -12,12 +12,18 @@ import { ref } from "vue";
 const showModal = ref(false);
 const newNote = ref("");
 const notes = ref([]);
+const errorMessage = ref("")
 
 function getRandomColor() {
   return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
 }
 
-const addNote = ()=>{
+
+  const addNote = ()=>{
+    if (newNote.value.length < 10) {
+      return errorMessage.value = "Your text must be upto 10 characters"
+    }
+
   notes.value.push({
     id: Math.floor(Math.random()* 10000000),
     text: newNote.value,
@@ -26,7 +32,11 @@ const addNote = ()=>{
 })
   showModal.value = false;
   newNote.value = "";
+  errorMessage.value = ""
 }
+
+
+
 
 </script>
 
@@ -37,7 +47,7 @@ const addNote = ()=>{
       <div class="flex justify-between p-4">
         <h2 class="text-3xl">Notes </h2>
 
-        <button @click="showModal = true" data-modal-target="staticModal" data-modal-toggle="staticModal"
+        <button @click="showModal = true" 
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           <svg
@@ -76,26 +86,27 @@ const addNote = ()=>{
     <!-- Modal  -->
 
   <!-- Main modal -->
-  <div v-show="showModal" id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-      <div class="relative w-full h-full max-w-2xl md:h-auto">
+  <div v-show="showModal"  class="fixed z-50 w-full bg-slate-400 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+      <div class="relative w-full h-full max-w-2xl mx-auto my-32">
           <!-- Modal content -->
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 mx-auto">
               <!-- Modal header -->
               <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                      Add New Note
                   </h3>
-                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
                       <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
                   </button>
               </div>
               <!-- Modal body -->
               <div class="p-6">
                   <textarea cols="64" rows="8" v-model="newNote"></textarea>
+                  <p v-if="errorMessage">{{ errorMessage }}</p>
               </div>
               <!-- Modal footer -->
               <div class="flex justify-end pr-6 pb-8 border-gray-200 rounded-b dark:border-gray-600">
-                  <button @click="addNote" data-modal-hide="staticModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Note</button>
+                  <button @click="addNote"  type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Note</button>
               </div>
           </div>
       </div>
